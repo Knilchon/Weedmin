ef3ada2f8eefad758c6d7d03d7e3293c06a593e3
Andrej Draca <andrej.draca@axelspringer.com>
2023-11-24 19:18:58 +0100

    port fix

--- index.js
+++ index.js
@@ -18,4 +18,5 @@ app.get("/image-4.png", (req,res) => {
   res.sendFile(imagePath);
 })
 
-app.listen(80, () => console.log("Server life Works!"));
+app.listen(80, () => console.log("Server life Works on Port 80!"));
+app.listen(443, () => console.log("Server life Works on Port 443!"));
