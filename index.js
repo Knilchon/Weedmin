32f12ef109828626a538dfc0a45b41f4c02910c3
Andrej Draca <andrej.draca@axelspringer.com>
2023-11-24 15:04:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,"/index.html"))
+})
+app.get("/image-4.png", (req,res) => {
+  const imagePath = path.join(__dirname, 'image-4.png');
+  
+  // Set the content type to indicate that it's an image
+  res.contentType('image/jpeg');
+
+  // Send the image file
+  res.sendFile(imagePath);
+})
+
+app.listen(8020, () => console.log("Server life Works!"));
